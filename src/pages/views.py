c229808e9bb05d93671adb24dd2b9fd75c6a1765
Nurzhigit <nurzigitturman@gmail.com>
2023-02-19 21:02:09 +0300

    Task3 add css and bootstrap, and work with him

--- src/pages/views.py
+++ src/pages/views.py
@@ -3,8 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 
 def home_screen_view(request):
-    print(request.headers)
-    return render(request, 'pages/home.html', {})
+        context = {}
+#     context['some_string'] = 'this is some string I am passing'
+#       context = {
+#                 'some_string': "this is some string I am passing"
+#         }
+
+        list_of_value = []
+        list_of_value.append("first")
+        list_of_value.append("second")
+        list_of_value.append("third")
+        list_of_value.append("fouth")
+
+        context['list_of_value'] = list_of_value
+        
+        return render(request, 'pages/home.html', context)
 
 
 
@@ -21,4 +34,5 @@ def error_403(request, exception):
         return render(request,'403.html')
 
 def error_400(request,  exception):
+        data = {}
         return render(request,'400.html', data) 
